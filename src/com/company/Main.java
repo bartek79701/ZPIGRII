3fc8b439aba7752a4b678223b80daa4fb49c8ab6
student <bartek7970@wp.pl>
2022-03-15 17:03:00 +0100

    Add Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("second branch");
+        System.out.println("second branch123");
 	// write your code here
     }
 }
